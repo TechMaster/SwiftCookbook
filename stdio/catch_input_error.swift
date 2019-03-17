3e049e8068cc86db9b52218083363c31db22841a
Techmaster <cuong@techmaster.vn>
2019-03-17 21:44:19 +0700

    catch_input_error.swift

--- stdio/catch_input_error.swift
+++ stdio/catch_input_error.swift
@@ -1,12 +1,27 @@
 //Read an integer and make sure it is valid number
 //Nếu chuỗi đọc vào không thể chuyển đổi sang integer
+//Cách 1:
 let a: Int? =  Int(readLine(strippingNewline: true)!)
 let b: Int? =  Int(readLine(strippingNewline: true)!)
 if a != nil && b != nil  {
 		print("\(a!) + \(b!) = \(a!+b!)")
 }
 
-//Cách 2: sử dụng try catch và custom error
+//Cách 2: sử dụng guard, nhập lỗi bỏ qua luôn
+func use_guard() {
+	guard let a = Int(readLine(strippingNewline: true)!) else {		
+			return
+	}
+
+	guard let b = Int(readLine(strippingNewline: true)!) else {		
+			return
+	}
+
+	print("\(a) + \(b) = \(a + b)")
+}
+use_guard()
+
+//Cách 3: sử dụng try catch và custom error
 enum MyError : Error {
    case conversionError(string: String)
 }
